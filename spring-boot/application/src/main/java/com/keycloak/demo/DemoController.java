1c1226fcf3bd7c2fc2552c42cd369d4878551d9e
ahmsay <ahmsay@gmail.com>
2023-10-26 23:19:35 +0300

    keycloak roles are converted to spring boot authorites

--- spring-boot/application/src/main/java/com/keycloak/demo/DemoController.java
+++ spring-boot/application/src/main/java/com/keycloak/demo/DemoController.java
@@ -1,6 +1,8 @@
 package com.keycloak.demo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,10 @@ public class DemoController {
         return String.format("Hello, %s!", jwt.getClaimAsString("preferred_username"));
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/protected/premium")
     public String premium(@AuthenticationPrincipal Jwt jwt) {
+        var auth = SecurityContextHolder.getContext().getAuthentication();
         return String.format("Hello, %s!", jwt.getClaimAsString("preferred_username"));
     }
 }
