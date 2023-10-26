d1db5a9ced7061dd71ba2b4584f342d0fcb99c47
ahmsay <ahmsay@gmail.com>
2023-10-26 22:24:54 +0300

    this one actually works

--- spring-boot/application/src/main/java/com/keycloak/demo/DemoController.java
+++ spring-boot/application/src/main/java/com/keycloak/demo/DemoController.java
@@ -1,13 +1,20 @@
 package com.keycloak.demo;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-    @GetMapping(path = "/customers")
-    public String index() {
-        return "didit";
+    @GetMapping("/")
+    public String index(@AuthenticationPrincipal Jwt jwt) {
+        return String.format("Hello, %s!", jwt.getClaimAsString("preferred_username"));
+    }
+
+    @GetMapping("/protected/premium")
+    public String premium(@AuthenticationPrincipal Jwt jwt) {
+        return String.format("Hello, %s!", jwt.getClaimAsString("preferred_username"));
     }
 }
--- spring-boot/application/src/main/java/com/keycloak/demo/SecurityConfig.java
+++ spring-boot/application/src/main/java/com/keycloak/demo/SecurityConfig.java
@@ -2,14 +2,9 @@ package com.keycloak.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -17,32 +12,11 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final KeycloakLogoutHandler keycloakLogoutHandler;
-
-    SecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
-        this.keycloakLogoutHandler = keycloakLogoutHandler;
-    }
-
     @Bean
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-    }
-
-    @Bean
-    public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated())
-                .oauth2Login(withDefaults())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
-                .logout(logout -> logout
-                        .addLogoutHandler(keycloakLogoutHandler)
-                        .logoutSuccessUrl("/"))
-                .build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
                 .build();
     }
 }
